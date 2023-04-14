1060c42ed2e9e817dd8e0563441d8e6e0145ba81
Warrenxzzz <97703249+Warrenxzzz@users.noreply.github.com>
2023-04-14 15:46:52 -0400

    Inserted code for a daemon thread.
    Time calculator.

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Start")
+import datetime
+
+start_time = datetime.datetime.now()
+
+duration = datetime.datetime.now()-start_time
+print("anon delving ", duration.seconds//60, " minutes. Keep grinding...")
